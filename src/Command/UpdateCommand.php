9e2e03bee1fa7abbc925276920a4d8e4d57442db
jColfej <contact@colfej.fr>
2017-07-11 21:48:24 +0200

    update

--- src/Command/UpdateCommand.php
+++ src/Command/UpdateCommand.php
@@ -2,27 +2,37 @@
 
 namespace Colfej\LeKioskCLI\Command;
 
-use Herrera\Phar\Update\Manager;
-use Herrera\Phar\Update\Manifest;
+use Humbug\SelfUpdate\Updater;
+
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class UpdateCommand extends Command
-{
-    const MANIFEST_FILE = 'https://jcolfej.github.io/LeKiosk-cli/latest/manifest.json';
+class UpdateCommand extends Command {
 
-    protected function configure()
-    {
-        $this
-            ->setName('update')
-            ->setDescription('Self-update to the latest version')
-        ;
+    const LAST_PHAR_URL = 'https://jcolfej.github.io/LeKiosk-cli/latest/lekiosk-cli.phar';
+    const LAST_VERSION_URL = 'https://jcolfej.github.io/LeKiosk-cli/latest/version';
+
+    protected function configure() {
+        $this->setName('update');
+        $this->setDescription('Self-update to the latest version');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $manager = new Manager(Manifest::loadFile(self::MANIFEST_FILE));
-        $manager->update($this->getApplication()->getVersion(), true);
+    protected function execute(InputInterface $input, OutputInterface $output) {
+
+        $updater = new Updater();
+
+        $updater->getStrategy()->setPharUrl(self::LAST_PHAR_URL);
+        $updater->getStrategy()->setVersionUrl(self::LAST_VERSION_URL);
+
+        try {
+            $result = $updater->update();
+            echo $result ? "Updated!\n" : "No update needed!\n";
+        } catch (\Exception $e) {
+            echo "Well, something happened! Either an oopsie or something involving hackers.\n";
+            exit(1);
+        }
+        
     }
+
 }
