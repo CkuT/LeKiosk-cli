4eb5747ccea585b239d5dec22723e89ab6ebe0f4
Jordann <jcollin@rmn.com>
2017-07-11 22:28:41 +0200

    New

--- src/Command/UpdateCommand.php
+++ src/Command/UpdateCommand.php
@@ -14,8 +14,46 @@ class UpdateCommand extends Command {
     const LAST_VERSION_URL = 'https://jcolfej.github.io/LeKiosk-cli/latest/version';
 
     protected function configure() {
+
         $this->setName('update');
-        $this->setDescription('Self-update to the latest version');
+        $this->setDescription('Update to most recent stable, pre-release or development build.');
+
+        $this->addOption(
+                'dev',
+                'd',
+                InputOption::VALUE_NONE,
+                'Update to most recent development build of Humbug.'
+            );
+        $this->addOption(
+                'non-dev',
+                'N',
+                InputOption::VALUE_NONE,
+                'Update to most recent non-development (alpha/beta/stable) build.'
+            );
+        $this->addOption(
+                'pre',
+                'p',
+                InputOption::VALUE_NONE,
+                'Update to most recent pre-release version (alpha/beta/rc).'
+            );
+        $this->addOption(
+                'stable',
+                's',
+                InputOption::VALUE_NONE,
+                'Update to most recent stable version.'
+            );
+        $this->addOption(
+                'rollback',
+                'r',
+                InputOption::VALUE_NONE,
+                'Rollback to previous version if available on filesystem.'
+            );
+        $this->addOption(
+                'check',
+                'c',
+                InputOption::VALUE_NONE,
+                'Checks what updates are available across all possible stability tracks.'
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
